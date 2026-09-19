dd244262567d3c589bdce670210c8ee055534a8c
Hafidz <muazzam0107@gmail.com>
2026-09-19 10:26:33 +0700

    hapus field thumbnail dari experience model

--- main/models.py
+++ main/models.py
@@ -15,9 +15,9 @@ class Experience(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     category = models.CharField(max_length=20, choices=EXPERIENCE_CHOICES, default='full-time')
-    thumbnail = models.URLField(blank=True, null=True)
     started_at = models.DateTimeField(auto_now_add=True)
     ended_at = models.DateTimeField(blank=True, null=True)
+    
     def __str__(self):
         return self.title
     
